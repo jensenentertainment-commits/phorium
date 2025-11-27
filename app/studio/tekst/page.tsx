a5a829389bf4c742d3c266cbc972112b807bd5b4
Lars R. Jensen <jensen.entertainment@gmail.com>
2025-11-27 12:31:26 +0100

    loginupdate5

--- app/studio/tekst/page.tsx
+++ app/studio/tekst/page.tsx
@@ -4,11 +4,13 @@ import { Suspense } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import PhoriumTextForm from "@/app/components/PhoriumTextForm";
+import StudioAuthGate from "../StudioAuthGate";
 
 
 
 export default function TextPage() {
   return (
+     <StudioAuthGate>
     <motion.div
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
@@ -49,5 +51,6 @@ export default function TextPage() {
         <PhoriumTextForm />
       </Suspense>
     </motion.div>
+    </StudioAuthGate>
   );
 }
